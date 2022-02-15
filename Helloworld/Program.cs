02dae6ba5ca1cc970e181f4622ffabe48d8b51d6
Martin Raudkivi <mraudkiv@tktk.ee>
2022-02-15 21:48:14 +0200

    kooli töö

---
+++ Helloworld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string UserName;
+            UserName = Console.ReadLine();
+            Console.WriteLine("Hello! " + UserName);
+
+            Console.Read();
+        }
+    }
+}
